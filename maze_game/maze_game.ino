f1e937aff473b78cead1ee35b12c2b94a5c46e20
leerehwinkel512 <133400297+leerehwinkel512@users.noreply.github.com>
2024-02-24 10:02:06 -0600

    oled

--- maze_game/maze_game.ino
+++ maze_game/maze_game.ino
@@ -1,8 +1,8 @@
 #include "LedControl.h"
 
 //data, clock, chip select
-LedControl lc1=LedControl(11,13,12,8);
-LedControl lc2=LedControl(5,7,6,8);
+LedControl lc1=LedControl(10,12,11,8);
+LedControl lc2=LedControl(9,7,8,8);
 
 
 typedef struct {
@@ -11,6 +11,9 @@ typedef struct {
     int x;
     int y;    
 } LEDTuple;
+
+const int X_pin = 6; // analog pin connected to X output
+const int Y_pin = 7; // analog pin connected to Y output
 
 
 LEDTuple getMatrixAddress(int x, int y) {
@@ -128,6 +131,43 @@ LEDTuple getMatrixAddress(int x, int y) {
   return q;
 }
 
+void matrixSetLed(int x, int y, bool b){
+  LEDTuple w;
+  w = getMatrixAddress(x, y);
+
+  if(w.g == 1){
+    lc1.setLed(w.m, w.x, w.y, b);
+  }
+  if(w.g == 2){
+    lc2.setLed(w.m, w.x, w.y, b);
+  }
+}
+
+int checkWalls(int x, int y) {
+
+  // outside bounary check
+  if (x < 0) {
+    return 0;
+  }
+  if (x > 31) {
+    return 0;
+  }
+  if (y < 0) {
+    return 0;
+  }
+  if (y > 31) {
+    return 0;
+  }
+
+  // check maze wall
+//  if (mazeArray[x][y] == 1){
+//    Serial.println("wall: Maze");
+//    return 0;
+//  }
+
+  return 1;
+}
+
 void setup() {
 
   for(int index=0;index<lc1.getDeviceCount();index++) {
@@ -144,24 +184,62 @@ void setup() {
 
 }
 
+// starting position in maze
+int x_val = 16;
+int y_val = 16;
+int x_val_new = x_val;
+int y_val_new = y_val;
+
+
 void loop() {
 
-  LEDTuple w;
+  matrixSetLed(x_val,y_val,true);
+  delay(100);
 
-  for(int x=0; x<32; x++) {
-    for(int y=0; y<32; y++) {
-      w = getMatrixAddress(x, y);
+  x_val_new = x_val;
+  y_val_new = y_val;
 
-      if(w.g == 1){
-        lc1.setLed(w.m, w.x, w.y, true);
-        delay(50);
-        lc1.setLed(w.m, w.x, w.y, false);
-      }
-      if(w.g == 2){
-        lc2.setLed(w.m, w.x, w.y, true);
-        delay(50);
-        lc2.setLed(w.m, w.x, w.y, false);
-      }
+  if (analogRead(Y_pin) < 400) {
+    x_val_new = x_val_new + 1;
+    if (checkWalls(x_val_new, y_val_new) == 1){
+      //matrixSetLed(x_val,y_val,false);
+      x_val = x_val + 1;
+      x_val_new = x_val;
+      y_val_new = y_val;
+      matrixSetLed(x_val,y_val,true);
+    }
+  }
+
+  if (analogRead(Y_pin) > 700) {
+    x_val_new = x_val_new - 1;
+    if (checkWalls(x_val_new, y_val_new) == 1){
+      //matrixSetLed(x_val,y_val,false);
+      x_val = x_val - 1;
+      x_val_new = x_val;
+      y_val_new = y_val;
+      matrixSetLed(x_val,y_val,true);
+    }
+  }
+
+  if (analogRead(X_pin) < 400) {
+    y_val_new = y_val_new - 1;
+    if (checkWalls(x_val_new, y_val_new) == 1){
+      //matrixSetLed(x_val,y_val,false);
+      y_val = y_val - 1;
+      x_val_new = x_val;
+      y_val_new = y_val;
+      matrixSetLed(x_val,y_val,true);
+    }
+  }
+  
+  if (analogRead(X_pin) > 700) {
+    y_val_new = y_val_new + 1;
+    if (checkWalls(x_val_new, y_val_new) == 1){
+      //matrixSetLed(x_val,y_val,false);
+      y_val = y_val + 1;
+      x_val_new = x_val;
+      y_val_new = y_val;
+      matrixSetLed(x_val,y_val,true);
     }
   }
 }
